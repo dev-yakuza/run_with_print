d1109518f39d19c2b1c1a0563132e64ae940b50f
dev-yakuza <dev.yakuza@gmail.com>
2023-05-25 16:28:21 +0900

    feat: Add project

--- lib/run_with_print.dart
+++ lib/run_with_print.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/run_with_print_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/run_with_print_base.dart' show runWithPrint;
